126bbeb95cb5b42cf566282214f3dac777d9fabe
Prasanna026 <156639517+Prasanna026@users.noreply.github.com>
2024-01-15 15:48:50 -0500

    change5

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,10 +5,13 @@ import fibonacci from "./fib";
 export default (req, res) => {
   let result: string;
   const num: string = req?.params?.num;
-  const fibN: number = fibonacci(parseInt(num));
-  result = `fibonacci(${num}) is ${fibN}`;
-  if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+  const fibN: number | undefined = fibonacci(parseInt(num));
+  // result = `fibonacci(${num}) is ${fibN}`;
+  result = `fibonacci(${num}) is ${fibN !== undefined ? fibN : 'undefined'}`;
+  // if (fibN < 0) {
+  //   result = `fibonacci(${num}) is undefined`;
+  // }
+  if(res){
+    res?.send(result);
   }
-  res?.send(result);
 };
