f4a1603a6ab18226e4000338e48694d6e71b3c7e
Prasanna026 <156639517+Prasanna026@users.noreply.github.com>
2024-01-15 16:05:38 -0500

    change6

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,17 +1,39 @@
-// Endpoint for querying the fibonacci numbers
+// // Endpoint for querying the fibonacci numbers
 
+// import fibonacci from "./fib";
+
+// export default (req, res) => {
+//   let result: string;
+//   const num: string | undefined = req?.params?.num as string;
+//   const fibN: number = fibonacci(parseInt(num));
+//   result = `fibonacci(${num}) is ${fibN}`;
+//   // result = `fibonacci(${num}) is ${fibN !== undefined ? fibN : 'undefined'}`;
+//   if (fibN < 0) {
+//     result = `fibonacci(${num}) is undefined`;
+//   }
+//     res?.send(result);
+// };
+
+import { Request, Response } from 'express';
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   let result: string;
-  const num: string = req?.params?.num;
-  const fibN: number | undefined = fibonacci(parseInt(num));
-  // result = `fibonacci(${num}) is ${fibN}`;
-  result = `fibonacci(${num}) is ${fibN !== undefined ? fibN : 'undefined'}`;
-  // if (fibN < 0) {
-  //   result = `fibonacci(${num}) is undefined`;
-  // }
-  if(res){
-    res?.send(result);
+  const num: string | undefined = req?.params?.num as string;
+  const parsedNum: number = parseInt(num);
+
+  if (!isNaN(parsedNum)) {
+    const fibN: number = fibonacci(parsedNum);
+
+    if (fibN >= 0) {
+      result = `fibonacci(${num}) is ${fibN}`;
+    } else {
+      result = `fibonacci(${num}) is undefined`;
+    }
+  } else {
+    result = `Invalid input: ${num}`;
   }
+
+  res?.send(result);
 };
+
