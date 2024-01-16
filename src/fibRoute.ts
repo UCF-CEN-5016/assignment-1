a01aa82244a5d33228802acc65d807d6f767c5f2
Prasanna026 <156639517+Prasanna026@users.noreply.github.com>
2024-01-15 20:46:57 -0500

    new changes

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,25 +1,9 @@
-// // Endpoint for querying the fibonacci numbers
-
-// import fibonacci from "./fib";
-
-// export default (req, res) => {
-//   let result: string;
-//   const num: string | undefined = req?.params?.num as string;
-//   const fibN: number = fibonacci(parseInt(num));
-//   result = `fibonacci(${num}) is ${fibN}`;
-//   // result = `fibonacci(${num}) is ${fibN !== undefined ? fibN : 'undefined'}`;
-//   if (fibN < 0) {
-//     result = `fibonacci(${num}) is undefined`;
-//   }
-//     res?.send(result);
-// };
-
 import { Request, Response } from 'express';
 import fibonacci from "./fib";
 
 export default (req: Request, res: Response) => {
-  let result: string;
-  const num: string | undefined = req?.params?.num;
+  let result;
+  const num = req?.params?.num;
   const parsedNum: number = parseInt(num);
 
   if (!isNaN(parsedNum)) {
