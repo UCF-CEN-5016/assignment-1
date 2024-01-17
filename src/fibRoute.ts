b8f277d4c9fc72d21a84b6af0a25d40645c96945
ChanduBulusu1 <chandubulusu01@gmail.com>
2024-01-16 22:58:18 -0500

    Changed the variable type to number

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -2,14 +2,14 @@
 
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: { params: { num: string } }, res: { send: (result: string) => void }) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(num, 10)); // Parse as base 10
+  let result = fibonacci(${num}) is ${fibN};
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = fibonacci(${num}) is undefined;
   }
 
   res.send(result);
