446ff4b3606aaf12b7fbf10d3be87a1ecffdd143
mengchin-ucf <me688702@ucf.edu.>
2024-08-24 12:31:01 -0400

    Fix type errors in fib.ts and fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,13 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import { Request, Response } from "express"; // Add type annotations
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(num, 10)); // Ensure 'num' is parsed as an integer
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
