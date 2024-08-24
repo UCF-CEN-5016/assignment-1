bdbc128015856ce33745d97bd6069a1419970bcd
mengchin-ucf <me688702@ucf.edu.>
2024-08-24 19:24:07 -0400

    Fix ESLint warning in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -7,7 +7,7 @@ export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num, 10)); // Ensure 'num' is parsed as an integer
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
