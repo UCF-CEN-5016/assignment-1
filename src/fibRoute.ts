ae4dd2e28c38d430b133d0429057f1dea4cd6e84
Asritha0610 <asrithavallip@gmail.com>
2024-08-27 19:11:08 -0400

    removed the string in line number 11

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,13 +1,14 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import { Request, Response} from 'express';
 
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const num: string = req.params.num;
 
   const parsedNum: number = parseInt(num, 10);
   const fibN: number = fibonacci(parsedNum);
-  let result: string = `fibonacci(${parsedNum}) is ${fibN}`;
+  let result = `fibonacci(${parsedNum}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${parsedNum}) is undefined`;
