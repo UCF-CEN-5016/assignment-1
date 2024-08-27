19512005dce0146f5a9afacaab659078dd4be155
Asritha0610 <asrithavallip@gmail.com>
2024-08-27 18:35:58 -0400

    added the line to parse the string to a number

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -5,6 +5,7 @@ import fibonacci from "./fib";
 export default (req, res) => {
   const num: string = req.params.num;
 
+  const parsedNum: number = parseInt(num, 10);
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
