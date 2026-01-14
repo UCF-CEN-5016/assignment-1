83bc27788b5d48fbbbffde0b7ef7206a6a012960
ce181760 <ce181760@ucf.edu>
2026-01-14 15:50:06 -0500

    fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,16 +1,26 @@
 // util function that computes the fibonacci numbers
+/**
+ * Returns the first n Fibonacci numbers
+ * @param n Number of Fibonacci numbers to generate
+ * @returns number[] - array of numbers
+ */
 export function fibonacci(n: number): number[] {
   if (n < 1) {
     throw new Error("Input must be a positive integer");
   }
 
-  const fibNumbers: number[] = [];
+  const fibNumbers: number[] = []; // Explicitly typed
 
   for (let i = 0; i < n; i++) {
-    if (i === 0) fibNumbers.push(0);
-    else if (i === 1) fibNumbers.push(1);
-    else fibNumbers.push(fibNumbers[i - 1] + fibNumbers[i - 2]);
+    if (i === 0) {
+      fibNumbers.push(0);
+    } else if (i === 1) {
+      fibNumbers.push(1);
+    } else {
+      // Now TypeScript knows fibNumbers[i-1] and fibNumbers[i-2] are numbers
+      fibNumbers.push(fibNumbers[i - 1] + fibNumbers[i - 2]);
+    }
   }
 
-  return fibNumbers;
+  return fibNumbers; // Explicitly number[]
 }
