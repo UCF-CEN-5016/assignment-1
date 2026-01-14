1e79a53c0bd100c7144ea5d7fa2e8b6da39f7887
Cesar <ce181760@ucf.edu>
2026-01-14 11:57:52 -0500

    Fix ESLint errors in src/fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,11 @@
 // util function that computes the fibonacci numbers
-export default function fibonacci(n) {
-  if (n < 0) {
-    return -1;
-  } else if (n == 0) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
-  }
+export default function fibonacci(n: number): number {
+  if (n < 0) return -1;
+  else if (n === 0) return 0;
+  else if (n === 1) return 1;
 
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  const a: number = fibonacci(n - 1);
+  const b: number = fibonacci(n - 2);
+
+  return a + b;
 }
