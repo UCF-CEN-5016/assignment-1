41952d824444b117c44cc1ef44e461b9ba2e8e3a
Aisha Khwaja <khwajaa14@gmail.com>
2026-01-19 20:14:16 -0500

    Fix types in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,24 @@
 // Endpoint for querying the fibonacci numbers
-
+import type { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+type FibParams = { num: string };
+
+export default function fibRoute(req: Request<FibParams>, res: Response): void {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const n = Number.parseInt(num, 10);
 
-  if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+  // Prevent NaN from causing infinite recursion
+  if (Number.isNaN(n)) {
+    res.status(400).send(`fibonacci(${num}) is undefined`);
+    return;
   }
 
+  const fibN = fibonacci(n);
+  const result =
+    fibN < 0 ? `fibonacci(${num}) is undefined` : `fibonacci(${num}) is ${fibN}`;
+
   res.send(result);
-};
+}
+
